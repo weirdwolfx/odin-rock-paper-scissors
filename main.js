b8b2d6bd57d313195dd202af508ee59ec7f28772
weirdwolfx <aadityapgoyal@gmail.com>
2025-07-11 22:24:02 +0530

    Add styling to rps icons and edit main.js
    Removed logic code from main.js to help including DOM manipulation easier

--- main.js
+++ main.js
@@ -6,46 +6,8 @@ function getComputerChoice() {
     else return "scissors";
 }
 
-function getHumanChoice() {
-    const userInput = prompt("Your choice: ");
-    return String(userInput).toLowerCase();
-}
+const playGround = document.querySelector('.playGround');
 
-function playRound(humanChoice, computerChoice, humanScore, computerScore) {
-    if (humanChoice == computerChoice) {
-        console.log("Tie\n");
-    }
-    else if ((humanChoice == "rock" && computerChoice == "scissors") || 
-        (humanChoice == "paper" && computerChoice == "rock") || 
-        (humanChoice == "scissors" && computerChoice == "paper")) 
-    {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-        humanScore++;
-    }
-    else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-        computerScore++;
-    }
-    return `${humanScore + " " + computerScore}`;
-}
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-
-        const updatedScore = playRound(humanChoice, computerChoice, humanScore, computerScore);
-        const scoreArray = updatedScore.split(" ");
-        humanScore = parseInt(scoreArray[0]);
-        computerScore = parseInt(scoreArray[1]);
-    }
-
-    console.log(`Your score: ${humanScore}\n`);
-    console.log(`Computer Score: ${computerScore}\n`);
-}
-
-
-playGame();
+playGround.addEventListener('click', e => {
+    console.log(e.target.classList[1]);
+})
