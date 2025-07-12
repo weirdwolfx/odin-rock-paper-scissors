73abe8b4a9999b2f3843f9be1e20e9c66daa96e9
weirdwolfx <aadityapgoyal@gmail.com>
2025-07-12 21:25:51 +0530

    Add score update and working game logic
    Game goes on as long as page is not refreshed

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const MAX_SCORE = 5;
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
 
@@ -6,8 +8,41 @@ function getComputerChoice() {
     else return "scissors";
 }
 
+function updateScore() {
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+}
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) return;
+
+    if ((playerChoice == "rock" && computerChoice == "scissors") ||
+        (playerChoice == "paper" && computerChoice == "rock") ||
+        (playerChoice == "scissors" && computerChoice == "paper")) {
+        playerScore++;
+    }
+    else {
+        computerScore++;
+    }
+    
+    updateScore();
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+const playerScoreDisplay = document.querySelector('#player');
+const computerScoreDisplay = document.querySelector('#computer');
 const playGround = document.querySelector('.playGround');
 
+updateScore();
+
 playGround.addEventListener('click', e => {
-    console.log(e.target.classList[1]);
-})
+    const playerChoice = e.target.classList[1];
+    const computerChoice = getComputerChoice();
+
+    console.log(computerChoice, playerChoice);
+
+    playRound(playerChoice, computerChoice);
+});
+
