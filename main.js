d97719f20610c788833c633d41d3e4b7548db387
weirdwolfx <aadityapgoyal@gmail.com>
2025-07-13 18:02:23 +0530

    Add a menu to show the game log
    The menu will display the choices made by player and computer for all rounds

--- main.js
+++ main.js
@@ -3,23 +3,45 @@ const MAX_SCORE = 5;
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
 
-    if (choice == 0) return "rock";
-    else if (choice == 1) return "paper";
-    else return "scissors";
+    if (choice == 0) return "Rock";
+    else if (choice == 1) return "Paper";
+    else return "Scissors";
 }
 
 function decideWinner(playerChoice, computerChoice, playerScore, computerScore) {
-    if (playerChoice == computerChoice) return [playerScore, computerScore];
+    let winner = null;
 
-    if ((playerChoice == "rock" && computerChoice == "scissors") ||
-        (playerChoice == "paper" && computerChoice == "rock") ||
-        (playerChoice == "scissors" && computerChoice == "paper")) {
+    if (playerChoice == computerChoice) return [playerScore, computerScore, winner];
+
+    if ((playerChoice == "Rock" && computerChoice == "Scissors") ||
+        (playerChoice == "Paper" && computerChoice == "Rock") ||
+        (playerChoice == "Scissors" && computerChoice == "Paper")) {
+        winner = "player";
         playerScore++;
     }
     else {
+        winner = "computer";
         computerScore++;
     }
-    return [playerScore, computerScore];
+    return [playerScore, computerScore, winner];
+}
+
+function roundLog(playerChoice, computerChoice, winner) {
+    const playerChoiceDisplay = document.createElement('p');
+    const computerChoiceDisplay = document.createElement('p');
+
+    playerChoiceDisplay.textContent = playerChoice;
+    computerChoiceDisplay.textContent = computerChoice;
+
+    if (winner == 'player') {
+        playerChoiceDisplay.classList.add('winner');
+    }
+    else if (winner == 'computer') {
+        computerChoiceDisplay.classList.add('winner');
+    }
+
+    document.querySelector('.log.player').appendChild(playerChoiceDisplay);
+    document.querySelector('.log.computer').appendChild(computerChoiceDisplay);
 }
 
 function playGame() {
@@ -31,22 +53,24 @@ function playGame() {
     const playGround = document.querySelector('.playGround');
 
     function playRound(e) {
+        let winner = null;
+
         if (playerScore >= MAX_SCORE || computerScore >= MAX_SCORE) {
             playGround.removeEventListener('click', playRound);
             return;
         }
 
-        const playerChoice = e.target.classList[1];
+        const playerChoice = e.target.id;
         const computerChoice = getComputerChoice();
 
-        console.log(playerChoice, computerChoice);
-
-        [playerScore, computerScore] = decideWinner(playerChoice, computerChoice, playerScore, computerScore);
+        [playerScore, computerScore, winner] = decideWinner(playerChoice, computerChoice, playerScore, computerScore);
 
         playerScoreDisplay.textContent = playerScore;
         computerScoreDisplay.textContent = computerScore;
+
+        roundLog(playerChoice, computerChoice, winner);
     }
-    
+
     playGround.addEventListener('click', playRound);
 
 }
